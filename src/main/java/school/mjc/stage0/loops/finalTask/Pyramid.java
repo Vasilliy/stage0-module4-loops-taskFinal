fd791a5540764f51f78227fbe6fa0fc5b99f4352
Vasilliy <Vasily.vvk@gmail.com>
2022-11-10 18:24:25 +0300

    Done

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,13 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i =1; i<=cathetusLength;i++) {
+            for (int j=1; j<cathetusLength+i;j++){
+                if (i<=Math.absExact(cathetusLength-j)) System.out.print(" ");
+                else  System.out.print(((Math.abs(cathetusLength-j))+1));
+            }
+            System.out.println("");
+        }
     }
 
     public static void main(String[] args) {
